a8564adc6e0ff1bf86d77cc78c7ad1ae6d16af74
Kar2410 <unofficialcreator07@gmail.com>
2021-11-18 18:02:09 +0530

    team page

---
+++ team.php
@@ -0,0 +1,73 @@
+<?php include('path.php'); 
+include(ROOT_PATH . "/app/controllers/topics.php");
+
+
+?>
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+
+
+
+
+
+
+
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+
+<head>
+    
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Electra Society</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+     <!-- header css -->
+     <link rel="stylesheet" href="assets/css/style.css">
+
+
+  
+
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="apple-touch-icon" href="home/images/apple-touch-icon.png">
+    <link rel="shortcut icon" type="home/image/ico" href="home/images/elelogo.png" />
+
+    <!-- footer css -->
+    <link rel="stylesheet" href="assets/css/footer.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+
+
+</head>
+
+<body data-spy="scroll" data-target="#mainmenu" data-offset="50">
+
+   <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
+    <?php include(ROOT_PATH . "/app/includes/messages.php"); ?>
+
+
+    <section>
+
+           
+
+
+    </section>
+
+    
+  <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
+
+
+<!-- JQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Slick Carousel -->
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+<!-- Custom Script -->
+<script src="assets/js/scripts.js"></script>
+</body>
